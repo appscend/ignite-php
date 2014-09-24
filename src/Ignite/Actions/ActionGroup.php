67801d844e72095367cc0e9e69f1531b3bff7131
Răzvan Botea <razvan@appscend.com>
2014-09-24 16:55:27 +0300

    Added action group class which can be used in closures

---
+++ src/Ignite/Actions/ActionGroup.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Ignite\Actions;
+
+use Ignite\Action;
+
+class ActionGroup extends ActionBuffer{
+
+	public static function get($name, $prefix = '') {
+		$a = new Action('pag', [$name], $prefix);
+		self::$actionBuffer[] = $a;
+
+		return $a;
+	}
+
+} 
