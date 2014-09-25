60cedac1e92862fdd3927dc6081599263950c891
Răzvan Botea <razvan@appscend.com>
2014-09-25 11:46:42 +0300

    forgot semicolon in action name

--- src/Ignite/Actions/ActionGroup.php
+++ src/Ignite/Actions/ActionGroup.php
@@ -7,7 +7,7 @@ use Ignite\Action;
 class ActionGroup extends ActionBuffer{
 
 	public static function get($name, $prefix = '') {
-		$a = new Action('pag', [$name], $prefix);
+		$a = new Action('pag:', [$name], $prefix);
 		self::$actionBuffer[] = $a;
 
 		return $a;
